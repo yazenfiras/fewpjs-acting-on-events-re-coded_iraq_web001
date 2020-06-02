0b9914df5f519395e98a5e8da41bda168fbd9d40
yazen firas <psyazen3@gmail.com>
2020-06-02 17:17:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // Your code here
 
 let dodger = document.getElementById("dodger");
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
- 
-    dodger.style.left = `${left - 1}px`;
-  }
-});
+
 /*
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
