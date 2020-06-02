c0f03b4fa166ad89c44d1f1165f36579fb7fed92
yazen firas <psyazen3@gmail.com>
2020-06-02 17:09:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 
 let dodger = document.getElementById("dodger");
-
+/*
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
