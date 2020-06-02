c3762e2c6ca8edc29bdcdb7d804ad77ee25522c8
yazen firas <psyazen3@gmail.com>
2020-06-02 17:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let dodger = document.getElementById("dodger");
 
 
 function moveDodgerLeft() {
-if (e.key === "ArrowLeft" && left>0) {
+if (e.key === "ArrowLeft" || left>0) {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
-  dodger.style.left = `${left - 1}px`;
-   
-  }
-
+  
+    dodger.style.left = `${left - 1}px`;
+  
+}
 }
 
 function moveDodgerRight() {
