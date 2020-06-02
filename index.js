59a36b9b734d28a76b62198bbe77554a938e5a2e
yazen firas <psyazen3@gmail.com>
2020-06-02 17:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,15 @@
 
 let dodger = document.getElementById("dodger");
 
-
 function moveDodgerLeft() {
-    let leftNumbers = dodger.style.left.replace("px", "");
+  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
-if (e.key === "ArrowLeft" || left>0) {
-
-  
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`;
-  
+  }
 }
-}
+
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
@@ -24,15 +21,14 @@ function moveDodgerRight() {
   }
 }
 
-document.addEventListener("keydown", moveDodgerLeft());
- 
-   
-  
-
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
 });
-
